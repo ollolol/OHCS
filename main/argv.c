798cb439af8f5f1900f982b06fe5ebae46eeea09
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-21 22:42:33 +0800

    This can process arguments

--- main/argv.c
+++ main/argv.c
@@ -1,23 +1,33 @@
-#include "include/argv.h"
-auto char file = 0;
-extern char[] ReadFile;
-int argvCheck(char[] argv){
+#include "../include/argv.h"
+extern char file;
+extern char *ReadFile;
+int argvCheck(char* argv){
   switch (argv[0]){
       case '-':
-      argvCharCheck(argv[]);
+      argvCharCheck(argv);
+      break;
       default:
       if(file == 1){
-        *ReadFile = *argv;
+        ReadFile = argv;
+        file = 0;
       }
   }
+  return 0;
 }
-int argvCharCheck(char[] argv){
+int argvCharCheck(char* argv){
+  int b = 1;
   for(int a = 1;;a++){
     switch (argv[a]){
         case 'f':
         file = 1;
         break;
-        
+        case '\0':
+        b = 0;
+        break;
+    }
+    if(b){
+      break;
     }
   }
+  return 0;
 }
