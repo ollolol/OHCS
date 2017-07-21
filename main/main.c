ae2dee624ed8e28ff496d2ce26fc618053e8ad17
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-21 23:00:41 +0800

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,12 +3,15 @@
   Made by Hsiaosvideo
   2017/07/21
 */
-#include "include/main.h"
-
+#include "../include/main.h"
+#include "../include/argv.h"
+char file = 0;
+char R[100];
+char *ReadFile = R;
 int main(int argc, char* argv[]){
-  for(int a = argc;a--;){
-    if(argv[a] == "ohcs") break;
-    else 
+  for(int a = 0;a < argc;a++){
+    argvCheck(argv[a]);
   }
+  printf("%s\n",ReadFile);
   return 0;
 }
