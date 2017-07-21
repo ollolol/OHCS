66e0c782a3351611e1eb7f9e785cf01a2443d9fe
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-21 19:44:56 +0800

    Update argv.c

--- main/argv.c
+++ main/argv.c
@@ -1,4 +1,23 @@
 #include "include/argv.h"
-
-int argvCheck(char* argv){
+auto char file = 0;
+extern char[] ReadFile;
+int argvCheck(char[] argv){
+  switch (argv[0]){
+      case '-':
+      argvCharCheck(argv[]);
+      default:
+      if(file == 1){
+        *ReadFile = *argv;
+      }
+  }
+}
+int argvCharCheck(char[] argv){
+  for(int a = 1;;a++){
+    switch (argv[a]){
+        case 'f':
+        file = 1;
+        break;
+        
+    }
+  }
 }
