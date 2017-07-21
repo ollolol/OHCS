3d363c65ab79d99089ff7874b5d6aa751ea19f25
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-21 19:20:02 +0800

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,15 +3,12 @@
   Made by Hsiaosvideo
   2017/07/21
 */
-
-
 #include "include/main.h"
-
 
 int main(int argc, char* argv[]){
   for(int a = argc;a--;){
-  
-  
+    if(argv[a] == "ohcs") break;
+    else 
   }
   return 0;
 }
