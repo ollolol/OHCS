1f41f8f4947ec684ea12f39a3434e7c9c16ce2c8
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-22 11:59:17 +0800

    Update bigunsigned.c

--- main/bigunsigned.c
+++ main/bigunsigned.c
@@ -1,10 +1,11 @@
 
-#include <../include/bigunsigned.h>
-unsigned int* plus(unsigned int* a, uint32_t b){
-  unsigned int R_Value[128];
+
+#include "../include/bigunsigned.h"
+extern int R_Value[128];
+unsigned int* plus(unsigned int* a, unsigned int* b){
   unsigned int d;
   unsigned int carry = 0;
-  for(unsigned int c = 0, c < 128; c++){
+  for(unsigned int c = 0; c < 128; c++){
     R_Value[c] = a[c] + b[c] + carry;
     d = R_Value[c] % 10;
     carry = R_Value[c] - d;
@@ -14,14 +15,28 @@ unsigned int* plus(unsigned int* a, uint32_t b){
   return R_Value;
 }
 void printBig(unsigned int* a){
-  for(unsigned int b = 0; b < 128; b++){
+  for(unsigned int b = 0; b < a[127]; b++){
     printf("%u", a[b]);
   }
+  printf("\n");
 }
 void scanBig(unsigned int* a){
-  char str[128];
+  char str[127];
+
   scanf("%s", str);
-  for(unsigned int b = 0; b < 128; b++){    
+  printf("You enter: ");
+  int c = printf("%s\n", str);
+  --c;
+  if(c > 127){
+    return;
+  }
+  for(unsigned int b = 0; b < c; b++){
     a[b] = (unsigned int)str[b];
   }
+
+  for(unsigned int b = 0; b < c; b++){
+    a[b] -= 48;
+  }
+  a[127] = c;
 }
+
