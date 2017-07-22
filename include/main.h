d7735785e2e2668d810c5b6961d364076e03aaf6
TNPLR <30339558+TNPLR@users.noreply.github.com>
2017-07-22 10:20:00 +0800

    Update main.h

--- include/main.h
+++ include/main.h
@@ -1,4 +1,20 @@
 /*
+This file is part of Ollolol and Hsiaosvideo Cryptography System.
+
+    Ollolol and Hsiaosvideo Cryptography System is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    Ollolol and Hsiaosvideo Cryptography System is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ollolol and Hsiaosvideo Cryptography System.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
   This is the include file of main/main.c
 */
 #include <stdio.h>
